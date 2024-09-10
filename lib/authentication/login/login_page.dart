0604841c9c585707e7e26a33eb30ded8d7abaf7c
Gero Stoewe <gerostoewe@gmx.de>
2024-09-10 12:53:24 +0200

    changed:
    - now it is responsive

--- lib/authentication/login/login_page.dart
+++ lib/authentication/login/login_page.dart
@@ -25,11 +25,13 @@ class LoginPage extends StatelessWidget {
           children: [
             Column(
               children: [
-                Image.asset(
-                  'lib/images/auth_Img/login_wallpaper.jpg',
+                SizedBox(
                   width: size.width,
                   height: size.height * 0.45,
-                  fit: BoxFit.cover,
+                  child: Image.asset(
+                    'lib/authentication/login/images/login_wallpaper.jpg',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ],
             ),
@@ -47,124 +49,128 @@ class LoginPage extends StatelessWidget {
                     ),
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 5),
-                        Text(
-                            "Welcome to PlantFriends",
-                            style: Theme.of(context).textTheme.headlineMedium
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextField(
-                            controller: usernameController,
-                            icon: Icons.alternate_email_rounded,
-                            hintText: "Email Address",
-                            obscureText: false
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextField(
-                            controller: passwordController,
-                            icon: Icons.lock_outline_rounded,
-                            hintText: "Password",
-                            obscureText: true
-                        ),
-                        const SizedBox(height: 10),
-                        GestureDetector(
-                          onTap: () {},
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const SizedBox(height: 5),
+                          Text(
+                              "Welcome to PlantFriends",
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.headlineMedium
+                          ),
+                          const SizedBox(height: 20),
+                          CustomTextField(
+                              controller: usernameController,
+                              icon: Icons.alternate_email_rounded,
+                              hintText: "Email Address",
+                              obscureText: false
+                          ),
+                          const SizedBox(height: 20),
+                          CustomTextField(
+                              controller: passwordController,
+                              icon: Icons.lock_outline_rounded,
+                              hintText: "Password",
+                              obscureText: true
+                          ),
+                          const SizedBox(height: 10),
+                          GestureDetector(
+                            onTap: () {},
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text.rich(
+                                      TextSpan(
+                                          children: [
+                                            TextSpan(
+                                                text: "Forgot Password? ",
+                                                style: TextStyle(
+                                                    color: isDarkMode ? dmLightGrey : lmDarkGrey
+                                                )
+                                            ),
+                                            TextSpan(
+                                                text: "Reset Here",
+                                                style: TextStyle(
+                                                    color: Theme.of(context).hintColor
+                                                )
+                                            )
+                                          ]
+                                      )
+                                  ),
+                                ]
+                              ),
+                            ),
+                          const SizedBox(height: 15),
+                          CustomButton(
+                              onTap: login,
+                              text: "LOGIN"
+                          ),
+                          const SizedBox(height: 25),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
                               children: [
-                                Text.rich(
+                                Expanded(
+                                  child: Divider(
+                                    thickness: 0.5,
+                                    color: isDarkMode ? dmDarkGrey : lmLightGrey
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                                  child: Text(
+                                    "Or continue with",
+                                    style: TextStyle(
+                                      color: isDarkMode ? dmLightGrey : lmDarkGrey
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Divider(
+                                      thickness: 0.5,
+                                      color: isDarkMode ? dmDarkGrey : lmLightGrey
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 25),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SquareTile(imagePath: 'lib/authentication/login/images/google_logo.png'),
+                              SquareTile(imagePath: isDarkMode ? 'lib/authentication/login/images/apple_logo_dark_mode.png' : 'lib/authentication/login/images/apple_logo_light_mode.png'),
+                              SquareTile(imagePath: isDarkMode ? 'lib/authentication/login/images/x_logo_dark_mode.png' : 'lib/authentication/login/images/x_logo_light_mode.png'),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                                child: Text.rich(
                                     TextSpan(
                                         children: [
                                           TextSpan(
-                                              text: "Forgot Password? ",
+                                              text: "New to PlantFriends? ",
                                               style: TextStyle(
+                                                  fontSize: 18,
                                                   color: isDarkMode ? dmLightGrey : lmDarkGrey
                                               )
                                           ),
                                           TextSpan(
-                                              text: "Reset Here",
+                                              text: "Create Account",
                                               style: TextStyle(
+                                                  fontSize: 18,
                                                   color: Theme.of(context).hintColor
                                               )
                                           )
                                         ]
                                     )
                                 ),
-                              ]
                             ),
                           ),
-                        const SizedBox(height: 15),
-                        CustomButton(
-                            onTap: login,
-                            text: "LOGIN"
-                        ),
-                        const SizedBox(height: 25),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Divider(
-                                  thickness: 0.5,
-                                  color: isDarkMode ? dmDarkGrey : lmLightGrey
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                                child: Text(
-                                  "Or continue with",
-                                  style: TextStyle(
-                                    color: isDarkMode ? dmLightGrey : lmDarkGrey
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Divider(
-                                    thickness: 0.5,
-                                    color: isDarkMode ? dmDarkGrey : lmLightGrey
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 25),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const SquareTile(imagePath: 'lib/authentication/login/images/google_logo.png'),
-                            SquareTile(imagePath: isDarkMode ? 'lib/authentication/login/images/apple_logo_dark_mode.png' : 'lib/authentication/login/images/apple_logo_light_mode.png'),
-                            SquareTile(imagePath: isDarkMode ? 'lib/authentication/login/images/x_logo_dark_mode.png' : 'lib/authentication/login/images/x_logo_light_mode.png'),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Center(
-                              child: Text.rich(
-                                  TextSpan(
-                                      children: [
-                                        TextSpan(
-                                            text: "New to PlantFriends? ",
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                color: isDarkMode ? dmLightGrey : lmDarkGrey
-                                            )
-                                        ),
-                                        TextSpan(
-                                            text: "Create Account",
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                color: Theme.of(context).hintColor
-                                            )
-                                        )
-                                      ]
-                                  )
-                              ),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 )

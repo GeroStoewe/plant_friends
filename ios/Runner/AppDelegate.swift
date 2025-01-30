f80f4de9f687d0181507fcccc65c756d95178841
Aylin Oymak <aylin.oymak@safetyio.com>
2025-01-30 23:35:03 +0100

    Modified some configurations related to xcode to fix ios simulator problem

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    //FirebaseApp.configure() // Initialize Firebase //TODO Fix the issue with ios simulator notification
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }

6bc6a15c2fa84dd99ee03aaffd3f2dddca2d31e9
Aylin Oymak <aylin.oymak@safetyio.com>
2025-01-30 23:20:45 +0100

    Add try catch snytax to debug errors

--- lib/firebase/firebase_api.dart
+++ lib/firebase/firebase_api.dart
@@ -4,9 +4,21 @@ class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
   Future<void> initNotifications() async {
-    await _firebaseMessaging.requestPermission();
+    try {
+    await _firebaseMessaging.requestPermission(
+     // alert: true,
+     // badge: true,
+     // sound: true,
+    );
+
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token: $fCMToken');
+  } catch (e) {
+      print('Error initializing Firebase Messaging: $e');
+    }
   }
-
 }
+
+//TODO: fix issue with iphone.
+/// While initializing the app, there is a permission question asking for notifications.
+/// If you click "allow" on Iphone, the app does not work anymore. If you "deny", the app works as expected.
